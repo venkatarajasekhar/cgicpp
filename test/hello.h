7b7289d357a97e721fc72e71bff602b4a13d3057
chris@axistwelve.com <chris@axistwelve.com>
2013-08-15 12:14:13 +0100

    "1376565253 MULTIPLE FILES WORKING INTEST "

--- test/hello.h
+++ test/hello.h
@@ -1,1 +1,6 @@
+#include <string>
+#include <iostream>
+
+//using namespace std;
 void hello (const char * name);
+std::string cf_exec(const char * func_name);
--- test/main.c
+++ test/main.c
@@ -3,6 +3,7 @@
 int
 main (void)
 {
+  std::cout << cf_exec("pwd");
     hello ("world");
       return 0;
 }
