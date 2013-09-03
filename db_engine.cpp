ab48e5d1eeae52aaf992c60f536dd882f023300a
chris@axistwelve.com <chris@axistwelve.com>
2013-09-03 17:56:46 +0100

    "1378227406 dbenginetesting and chars "

---
+++ db_engine.cpp
@@ -0,0 +1,19 @@
+//#include "db_engine.h"
+//#include "index.h"
+#include <iostream>
+
+using namespace std;
+
+class db_Table {
+  private:
+    char filename;
+    char signal;
+  public: 
+    db_Table(const char* database, const char* table) {
+    }
+};
+
+int main() {
+
+  return 0;
+}
