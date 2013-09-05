6aa0b90072a992ad89324d5f609a8c03a54f6e3c
chris@axistwelve.com <chris@axistwelve.com>
2013-09-05 17:40:33 +0100

    "1378399233 db engine stuff, bus error 10 "

--- db_engine.cpp
+++ db_engine.cpp
@@ -1,6 +1,7 @@
 //#include "db_engine.h"
 //#include "index.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -9,22 +10,50 @@ class db_Table {
     char filename;
     bool status;
   public: 
-    db_Table(const char database[], const char* table) {
-      this->status = false;
+    ofstream* db_stream;
+    db_Table(const char* database, const char* table) {
+      ofstream db_stream(database);
+      this->db_stream = &db_stream;
+      if (db_stream.is_open()) {
+        this->status = true;
+      }
+      else {
+        this->status = false;
+      }
+    }
+    ~db_Table() {
+      this->close();
     }
     bool getStatus() {
       return this->status;
+    }
+    void close() {
+      if (this->db_stream != NULL) {
+        this->db_stream->close();
+      }
     }
 };
 
 int main() {
 
   db_Table db("db", "table");
-
-  if (db.getStatus() == false) {
-    cout << "FAILED";
-
-  }
+  *db.db_stream << "ASD";
+  db.close();
   
   return 0;
 }
+/*
+  string line;
+  while ( db.db_stream->good() )
+  {
+    getline (db.db_stream,line);
+    cout << line << endl;
+  }
+
+  if (db.getStatus() == false) {
+    cout << "FAILED";
+  }
+
+  return 0;
+}
+*/
